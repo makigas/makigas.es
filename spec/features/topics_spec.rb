01633be46d9eac45aa6e1530ef944f676d2618f1
Dani Rodríguez <danirod@outlook.com>
2016-08-08 17:15:32 +0200

    Let the user "browse" a topic
    There are no videos here.

--- spec/features/topics_spec.rb
+++ spec/features/topics_spec.rb
@@ -21,4 +21,28 @@ RSpec.feature "Topics", type: :feature do
       expect(page).to have_content @topic.description
     end
   end
+
+  context 'creating a topic' do
+    it 'should be success' do
+      visit new_topic_path
+      expect(page).to have_http_status :success
+    end
+
+    it 'should be able to insert data' do
+      visit topics_path
+      click_link 'Nueva temática'
+      fill_in 'Title', with: 'My sample topic'
+      fill_in 'Description', with: 'This topic is an example to test the feature'
+      attach_file 'Buscar', Rails.root + 'spec/fixtures/topic.jpg', visible: false
+      click_button 'Guardar'
+      expect(page).to have_content 'My sample topic'
+    end
+
+    it 'should detect invalid data' do
+      visit new_topic_path
+      fill_in 'Description', with: 'Look, no title!'
+      click_button 'Guardar'
+      expect(page).to have_content 'Hay errores que impiden guardar'
+    end
+  end
 end
