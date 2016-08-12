c4faabe6d4edffa0274f567aa738a9efa61452eb
Dani Rodríguez <danirod@outlook.com>
2016-08-12 23:48:01 +0200

    Translate: front#index

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,9 @@ module Makigas
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths << "#{Rails.root}/lib"
+
+    # Configure internationalization. What if one day I translate my channel?
+    config.i18n.default_locale = :es
+    config.i18n.load_path += Dir["#{Rails.root}/config/locales/**/*.yml"]
   end
 end
