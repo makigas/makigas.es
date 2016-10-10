fa6649f8543845f1cc459ea4d2db2de05afa4f82
Dani Rodríguez <danirod@outlook.com>
2016-10-10 20:48:13 +0200

    Remove old edition panels

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -10,46 +10,4 @@ class PlaylistsController < ApplicationController
       redirect_to @playlist, status: 301
     end
   end
-
-  def new
-    @playlist = Playlist.new
-  end
-
-  def create
-    @playlist = Playlist.new(playlist_attributes)
-    if @playlist.save
-      redirect_to playlist_path(@playlist)
-    else
-      render :new
-    end
-  end
-
-  def edit
-    @playlist = Playlist.friendly.find(params[:id])
-  end
-
-  def update
-    @playlist = Playlist.friendly.find(params[:id])
-    if @playlist.update_attributes(playlist_attributes)
-      redirect_to playlist_path(@playlist)
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @playlist = Playlist.friendly.find(params[:id])
-    if @playlist.destroy
-      redirect_to playlists_path, notice: 'Borrado con éxito'
-    else
-      raise Errors::UnprocessableEntity
-    end
-  end
-
-  private
-
-  def playlist_attributes
-    params.require(:playlist).permit(:title, :description, :youtube_id, :photo)
-  end
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'front#index'
 
-  # These routes should be limited to authorized users.
-  resources :playlists, path: 'series', only: [:new, :create, :edit, :update, :destroy] do
-    resources :videos, path: '/', only: [:edit, :update, :destroy]
-  end
-  resources :videos, only: [:new, :create]
-  resources :topics, only: [:new, :create, :edit, :update, :destroy] do
-    member do
-      get :insert, to: 'topics#insert'
-      post :insert, to: 'topics#do_insert'
-      delete :release, to: 'topics#release'
-    end
-  end
-  
-  # These are the public routes for my application.
   get :videos, to: 'videos#index'
   resources :playlists, path: 'series', only: [:index, :show] do
     resources :videos, path: '/', only: [:show]
