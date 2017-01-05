c6e49ff3eff19a7505ab18d42919f042e19203d3
Dani Rodríguez <danirod@outlook.com>
2017-01-05 19:48:52 +0100

    Add a front page

--- app/controllers/dashboard/dashboard_controller.rb
+++ app/controllers/dashboard/dashboard_controller.rb
@@ -1,2 +1,10 @@
 class Dashboard::DashboardController < ApplicationController
+
+  def index
+    @videos = Video.count
+    @playlists = Playlist.count
+    @topics = Topic.count
+    @users = User.count
+  end
+
 end
