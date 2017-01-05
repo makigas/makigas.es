06edc29031385c4219e0b021ae64dd8724ca2f81
Dani Rodríguez <danirod@outlook.com>
2017-01-06 00:23:28 +0100

    Add a filter bar to the videos page

--- app/assets/javascripts/videos.js
+++ app/assets/javascripts/videos.js
@@ -8,4 +8,8 @@ $(document).ready(function() {
     $('.video-player').toggleClass('col-md-8').toggleClass('col-md-12');
     $('.video-description').toggleClass('hidden-md').toggleClass('hidden-lg');
   });
+
+  $('body.page-videos form#videos_filter fieldset[data-autosearch] input').on('change', function(e) {
+    $(this).closest('form').submit();
+  });
 })
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,14 @@
 class VideosController < ApplicationController
 
   def index
-    @videos = Video.all.order(created_at: :desc).page(params[:page]).per 10
+    @videos = Video.joins(:playlist).all
+    if params[:length]
+      @videos = @videos.where('duration <= 300') if params[:length] == "short"
+      @videos = @videos.where('duration > 300 and duration <= 900') if params[:length] == "medium"
+      @videos = @videos.where('duration > 900') if params[:length] == "long"
+    end
+    @videos = @videos.where(playlists: { topic_id: Topic.where(slug: params[:topic]).pluck(:id) }) if params[:topic]
+    @videos = @videos.order(created_at: :desc).page(params[:page]).per 10
   end
 
   def show
