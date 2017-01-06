7648bd3b5eba3e2ad8f954b004018daa3b71929d
Dani Rodríguez <danirod@outlook.com>
2017-01-06 23:45:19 +0100

    Set default_url_options for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "makigas_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'www.makigas.es', port: 3000, protocol: 'https' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
