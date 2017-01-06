b8da59f41355fed271ae27ffac99adfc47fb2dce
Dani Rodríguez <danirod@outlook.com>
2017-01-06 19:26:09 +0100

    Thumbnails are no longer mandatory
    It is too unconvenient at the moment if I'm going to do a mass migration
    with the feature implemented in this branch. After the thumbnails are
    uploaded in upstream, I may consider making them mandatory by adding the
    presence validation again and by making the database fields NOT NULL.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -8,13 +8,12 @@ class Topic < ApplicationRecord
     small: "180x180>",
     default: "360x360>",
     hidef: "720x720>"
-  }, default_url: "defaults/topics/:style.png"
+  }, default_url: "/makigas.png"
 
   validates :title, presence: true, length: { maximum: 50 }
   validates :description, presence: true, length: { maximum: 250 }
   validates :color, presence: true
-  validates_attachment :thumbnail, presence: true,
-    content_type: { content_type: /\Aimage\/.*\z/ }
+  validates_attachment :thumbnail, content_type: { content_type: /\Aimage\/.*\z/ }
 
   # Playlists can survive without a topic, so on delete set the topic to null.
   has_many :playlists, dependent: :nullify
