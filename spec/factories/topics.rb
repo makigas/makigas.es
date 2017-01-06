1cc766af7f88c9e834c82888a668de87f3fb0b48
Dani Rodríguez <danirod@outlook.com>
2017-01-06 19:07:09 +0100

    Update test cases

--- spec/factories/topics.rb
+++ spec/factories/topics.rb
@@ -2,5 +2,7 @@ FactoryGirl.define do
   factory :topic do
     title 'Popular musics'
     description 'Popular musics of all kinds of styles and genres'
+    thumbnail { fixture_file_upload(Rails.root.join('spec/fixtures/topic.png'), 'image/png') }
+    color '#55ff55'
   end
 end
