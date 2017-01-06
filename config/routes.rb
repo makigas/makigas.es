92e9fb9f565371feb57e00b9bb9026867139b98b
Dani Rodríguez <danirod@outlook.com>
2017-01-07 00:20:08 +0100

    Redirect old routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   authenticate :user do
     namespace :dashboard do
       root to: 'dashboard#index', as: ''
-
       resources :topics
       resources :videos, only: [:index, :new, :create]
       resources :playlists do
@@ -27,9 +26,14 @@ Rails.application.routes.draw do
   get :disclaimer, to: 'pages#disclaimer'
   get :cookies, to: 'pages#cookies'
 
-  get :videos, to: 'videos#index', constraints: { format: :html }
+  resources :videos, only: :index, constraints: { format: :html }
   resources :playlists, path: 'series', only: [:index, :show], constraints: { format: :html } do
     resources :videos, path: '/', only: [:show], constraints: { format: :html }
   end
   resources :topics, only: [:index, :show], constraints: { format: :html }
+
+  # Redirect old routes.
+  get '/videos/:topic/:playlist/episodio/:video' => redirect('/series/%{playlist}/%{video}')
+  get '/videos/:topic/:playlist' => redirect('/series/%{playlist}')
+  get '/videos/:playlist' => redirect('/series/%{playlist}')
 end
