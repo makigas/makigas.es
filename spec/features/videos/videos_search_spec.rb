35d4be049fa92b6db3e1f7263a7b72c7bc6b2b2d
Dani Rodríguez <danirod@outlook.com>
2017-02-04 20:06:09 +0100

    Test search page for videos

---
+++ spec/features/videos/videos_search_spec.rb
@@ -0,0 +1,74 @@
+require 'rails_helper'
+
+RSpec.feature "Videos search", type: :feature do
+  context "searching by length" do
+    before(:each) do
+      @short = FactoryGirl.create(:video, duration: 200, title: 'Short', youtube_id: '1')
+      @medium = FactoryGirl.create(:video, duration: 600, title: 'Medium', youtube_id: '2')
+      @long = FactoryGirl.create(:video, duration: 1200, title: 'Long', youtube_id: '3')
+    end
+    
+    scenario "can search for short length videos" do
+      visit videos_path
+      choose 'Cortos'
+      click_button 'Aplicar filtros'
+      expect(page).to have_link @short.title, href: video_path(@short)
+      expect(page).not_to have_link @medium.title, href: video_path(@medium)
+      expect(page).not_to have_link @long.title, href: video_path(@long)
+    end
+
+    scenario "can search for medium length videos" do
+      visit videos_path
+      choose 'Medios'
+      click_button 'Aplicar filtros'
+      expect(page).not_to have_link @short.title, href: video_path(@short)
+      expect(page).to have_link @medium.title, href: video_path(@medium)
+      expect(page).not_to have_link @long.title, href: video_path(@long)
+    end
+
+    scenario "can search for long length videos" do
+      visit videos_path
+      choose 'Largos'
+      click_button 'Aplicar filtros'
+      expect(page).not_to have_link @short.title, href: video_path(@short)
+      expect(page).not_to have_link @medium.title, href: video_path(@medium)
+      expect(page).to have_link @long.title, href: video_path(@long)
+    end
+  end
+
+  context "searching by topic" do
+    before(:each) do
+      @topic1 = FactoryGirl.create(:topic, title: 'First Topic')
+      @topic2 = FactoryGirl.create(:topic, title: 'Second Topic')
+      @playlist1 = FactoryGirl.create(:playlist, topic: @topic1)
+      @playlist2 = FactoryGirl.create(:playlist, topic: @topic2)
+      @video1 = FactoryGirl.create(:video, title: 'First', youtube_id: 'A', playlist: @playlist1)
+      @video2 = FactoryGirl.create(:video, title: 'Second', youtube_id: 'B', playlist: @playlist2)
+    end
+
+    scenario "can search for videos in a topic" do
+      visit videos_path
+
+      check 'First Topic'
+      click_button 'Aplicar filtros'
+      expect(page).to have_link @video1.title, href: video_path(@video1)
+      expect(page).not_to have_link @video2.title, href: video_path(@video2)
+    end
+
+    scenario "can search for videos in multiple topics" do
+      visit videos_path
+
+      check 'First Topic'
+      check 'Second Topic'
+      click_button 'Aplicar filtros'
+      expect(page).to have_link @video1.title, href: video_path(@video1)
+      expect(page).to have_link @video2.title, href: video_path(@video2)
+    end
+  end
+
+  private
+
+  def video_path(video)
+    playlist_video_path(video, playlist_id: video.playlist)
+  end
+end
