67bd3d8c4eb58d1d1787795d34192a3c2b93853e
Dani Rodríguez <danirod@outlook.com>
2017-02-20 00:49:32 +0100

    Install Capistrano

---
+++ Capfile
@@ -0,0 +1,19 @@
+# Load DSL and set up stages
+require "capistrano/setup"
+
+# Include default deployment tasks
+require "capistrano/deploy"
+
+# Load the SCM plugin appropriate to your project:
+require "capistrano/scm/git"
+install_plugin Capistrano::SCM::Git
+
+# Include tasks from other gems included in your Gemfile
+require "capistrano/rvm"
+require "capistrano/bundler"
+require "capistrano/rails/assets"
+require "capistrano/rails/migrations"
+require "capistrano/passenger"
+
+# Load custom tasks from `lib/capistrano/tasks` if you have any defined
+Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
---
+++ config/deploy.rb
@@ -0,0 +1,22 @@
+# config valid only for current version of Capistrano
+lock "3.7.2"
+
+# Keep sensitive deployment informatino out of the repo. As seen on:
+# https://github.com/AyuntamientoMadrid/transparencia/blob/master/config/deploy.rb
+def deploy_param(key)
+  @deploy_keys ||= YAML.load_file('config/deploy.yml')[fetch(:stage).to_s]
+  @deploy_keys.fetch(key.to_s, 'undefined')
+end
+
+# Deploy settings
+set :application, "makigas"
+set :deploy_to, deploy_param(:deploy_to)
+set :repo_url, deploy_param(:repo_url)
+
+# Shared settings
+append :linked_files, "config/database.yml", "config/secrets.yml", "public/sitemap.xml.gz"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Misc settings
+after 'deploy:finishing', 'deploy:cleanup'
+set :passenger_restart_with_touch, true
