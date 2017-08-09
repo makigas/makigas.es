d8e8eeea932b40542ae801f071de292200f47a25
Dani Rodríguez <dani@danirod.es>
2017-08-09 19:06:57 +0200

    Use Yarn for pulling cookieconsent2
    Note that cookieconsent v3 is out so I should consider upgrading.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require bootstrap/dist/js/bootstrap.min
 //= require jquery_ujs
 //= require_directory .
+//= require cookieconsent2/build/cookieconsent.min
 
 $(document).ready(function() {
   // Tell the stylesheet that JavaScript is on.
