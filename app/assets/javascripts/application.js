1e39600f557d59487f3ca95d0affaa85cf55a2c3
Dani Rodríguez <dani@danirod.es>
2017-08-10 12:50:14 +0200

    Update front-end dependencies (#54)
    This commit updates jQuery to 3.x given that Bootstrap 3.3.7 already
    supports it and it upgrades Cookie Consent v2 to v3. This
    additionally reduces the amount of dependencies required on the
    front-end because Cookie Consent 3 has less dependencies than Cookie
    Consent 2.
    
    * jquery, 1.12.4 ~> 3.2.1
    * cookieconsent2, 1.10.10-2 ~> (removed)
    * cookieconsent, (new) ~> 3.0.4

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,7 @@
 //= require bootstrap/dist/js/bootstrap.min
 //= require jquery_ujs
 //= require_directory .
-//= require cookieconsent2/build/cookieconsent.min
+//= require cookieconsent/build/cookieconsent.min
 
 $(document).ready(function() {
   // Tell the stylesheet that JavaScript is on.
