b0f459a5e9f3e85c9bf7ce2cd1020b555d49d043
Dani Rodríguez <dani@danirod.es>
2017-08-11 22:14:57 +0200

    Move production config for Puma
    Apparently Puma automatically selects the appropiate configuration file
    for the environment if it's on config/puma/$RACK_ENV.rb. In this case,
    moving the Puma configuration to config/puma/production.rb should make
    it when running the application in `production`.

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,3 @@
-# This is the default Puma configuration optimized for development
-# If you are trying to run Puma in a production environment, please
-# use puma.production.rb (such as bin/puma -C puma.production.rb).
-
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,8 +1,3 @@
-# This is a different Puma configuration optimized for production.
-# Main differences against the default development settings:
-# - Uses workers for multiprocessor servers.
-# – Binds to an UNIX socket instead of to a TCP port.
-
 workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
