3af011f44db4b7b8d2bf922631bb810578e39695
Dani Rodríguez <dani@danirod.es>
2017-08-11 22:04:30 +0200

    Adds a production Puma configuration

---
+++ config/puma.production.rb
@@ -0,0 +1,26 @@
+# This is a different Puma configuration optimized for production.
+# Main differences against the default development settings:
+# - Uses workers for multiprocessor servers.
+# – Binds to an UNIX socket instead of to a TCP port.
+
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads threads_count, threads_count
+
+preload_app!
+
+environment ENV.fetch("RAILS_ENV") { "development" }
+
+bind 'unix://tmp/sockets/puma.sock'
+pidfile 'tmp/pids/puma.pid'
+state_path 'tmp/pids/puma.state'
+
+before_fork do
+  ActiveRecord::Base.connection_pool.disconnect!
+end
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
+
+plugin :tmp_restart
