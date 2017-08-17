93701168ef6bd356c9b4e2b5fcd1ad296ed9dff3
Dani Rodríguez <dani@danirod.es>
2017-08-17 11:46:46 +0200

    Fixes regression on timer component for videos
    Apparently this broke, or was already broken. Setting the hours, minutes
    and seconds was not properly triggering the JavaScript events that
    update the actual duration in seconds for this field.
    
    The JavaScript event has been replaced with a new submit event that
    updates the value for the duration in seconds just before submitting the
    form. Alternatively, it has been thought to simply make the backend
    controller accept the duration sexagesimal components.

--- app/assets/javascripts/dashboard/videos.js
+++ app/assets/javascripts/dashboard/videos.js
@@ -1,46 +1,16 @@
 window.addEventListener('load', function() {
   if (!document.body.classList.contains('page-videos')) return;
 
+  /* On form submission, update the video duration hidden field. */
   var form = document.querySelector('form.simple_form');
+  form && form.addEventListener('submit', function() {
+    // Extract sexagesimal time values.
+    var hh = form.querySelector('#duration_hours').value || 0;
+    var mm = form.querySelector('#duration_minutes').value || 0;
+    var ss = form.querySelector('#duration_seconds').value || 0;
 
-  // Check that there is actually a form in this page.
-  if (form == null) return;
-
-  /**
-   * Updates the hidden duration field using the sexagesimal values.
-   * Because setting the duration using seconds would be hard when the
-   * video lasts more than 60 seconds, the duration is set using hours,
-   * minutes and seconds by the user. This function will update the
-   * hidden duration value which is the value actually sent to the
-   * server.
-   */
-  var updateDuration = function() {
-    // Get the current sexagesimal time.
-    var hours = parseInt(form.querySelector('#duration_hours').value) || 0;
-    var minutes = parseInt(form.querySelector("#duration_minutes").value) || 0;
-    var seconds = parseInt(form.querySelector("#duration_seconds")) || 0;
-
-    // Update the hidden actual seconds value.
-    var duration = hours * 3600 + minutes * 60 + seconds;
-    form.querySelector('#video_duration').value = duration;
-  };
-
-  [
-    form.querySelector("#duration_hours"),
-    form.querySelector("#duration_minutes"),
-    form.querySelector("#duration_seconds")
-  ].forEach(function(item) {
-    item.addEventListener('change', updateDuration);
-    item.addEventListener('keyup', updateDuration);
-    item.addEventListener('blur', function(e) {
-      if (e.target.value == "") {
-        e.target.value = 0;
-      } else {
-        var realValue = parseInt(e.target.value);
-        if (realValue < 0) {
-          e.target.value = 0;
-        }
-      }
-    });
+    // Convert to seconds.
+    var time = parseInt(hh) * 3600 + parseInt(mm) * 60 + parseInt(ss);
+    form.querySelector('#video_duration').value = time;
   });
 });
