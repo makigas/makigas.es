74dd78ed79b9aef6b2845877feb66ce58a762cd5
Dani Rodríguez <dani@danirod.es>
2017-08-20 00:31:17 +0200

    Fix dashboard front-end shenanigans (#64)
    Apparently this broke, or was already broken. Setting the hours, minutes
    and seconds was not properly triggering the JavaScript events that
    update the actual duration in seconds for this field.
    
    The JavaScript event has been replaced with a new submit event that
    updates the value for the duration in seconds just before submitting the
    form. Alternatively, it has been thought to simply make the backend
    controller accept the duration sexagesimal components.
    
    The duration component in the video form actually requires testing that
    the JavaScript code the page uses to convert the duration from
    sexagesimal units to seconds actually works.
    
    In addition, this commit adds support for Poltergeist, a library that makes
    use of PhantomJS to run some feature tests in a real browser with
    JavaScript support, unlike the default browser used by Capybara.

--- app/assets/javascripts/dashboard/videos.js
+++ app/assets/javascripts/dashboard/videos.js
@@ -1,46 +1,19 @@
-window.addEventListener('load', function() {
-  if (!document.body.classList.contains('page-videos')) return;
+/*
+ * On form submission, the sexagesimal components for the video
+ * length should be extracted and the duration in seconds should be
+ * calculated, as that is what has to be sent to the backend.
+ */
+var form = document.querySelector('body.page-videos form.simple_form');
 
-  var form = document.querySelector('form.simple_form');
+if (form) {
+  form.addEventListener('submit', function(e) {
+    // Extract sexagesimal time values.
+    var hh = form.querySelector('#duration_hours').value || 0;
+    var mm = form.querySelector('#duration_minutes').value || 0;
+    var ss = form.querySelector('#duration_seconds').value || 0;
 
-  // Check that there is actually a form in this page.
-  if (form == null) return;
-
-  /**
-   * Updates the hidden duration field using the sexagesimal values.
-   * Because setting the duration using seconds would be hard when the
-   * video lasts more than 60 seconds, the duration is set using hours,
-   * minutes and seconds by the user. This function will update the
-   * hidden duration value which is the value actually sent to the
-   * server.
-   */
-  var updateDuration = function() {
-    // Get the current sexagesimal time.
-    var hours = parseInt(form.querySelector('#duration_hours').value) || 0;
-    var minutes = parseInt(form.querySelector("#duration_minutes").value) || 0;
-    var seconds = parseInt(form.querySelector("#duration_seconds")) || 0;
-
-    // Update the hidden actual seconds value.
-    var duration = hours * 3600 + minutes * 60 + seconds;
-    form.querySelector('#video_duration').value = duration;
-  };
-
-  [
-    form.querySelector("#duration_hours"),
-    form.querySelector("#duration_minutes"),
-    form.querySelector("#duration_seconds")
-  ].forEach(function(item) {
-    item.addEventListener('change', updateDuration);
-    item.addEventListener('keyup', updateDuration);
-    item.addEventListener('blur', function(e) {
-      if (e.target.value == "") {
-        e.target.value = 0;
-      } else {
-        var realValue = parseInt(e.target.value);
-        if (realValue < 0) {
-          e.target.value = 0;
-        }
-      }
-    });
+    // Convert to seconds.
+    var time = parseInt(hh) * 3600 + parseInt(mm) * 60 + parseInt(ss);
+    form.querySelector('#video_duration').value = time;
   });
-});
+}
