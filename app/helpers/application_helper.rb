ab2dd7abf15eefc552994822f0788fc234382f5f
Dani Rodríguez <dani@danirod.es>
2017-08-20 00:36:39 +0200

    Remove dead code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -46,17 +46,4 @@ module ApplicationHelper
     markdown = Redcarpet::Markdown.new(render)
     markdown.render(text).html_safe
   end
-  
-  # TODO: Remove this method after it has been tested that is not used.
-  def horizontal_form_for(resource, options = {}, &block)
-    options[:html] = { class: 'form-horizontal' }
-    options[:wrapper] = :horizontal_form
-    options[:wrapper_mappings] = {
-      check_boxes: :horizontal_radio_and_checkboxes,
-      radio_buttons: :horizontal_radio_and_checkboxes,
-      file: :horizontal_file_input,
-      boolean: :horizontal_boolean
-    }
-    simple_form_for(resource, options, &block)
-  end
 end
