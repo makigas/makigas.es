822e7bc7ebc7e1bfd5fc805b0e7f9c0680bb2685
Dani Rodríguez <dani@danirod.es>
2017-12-06 19:27:38 +0100

    Add ATOM syndication feeds to the site (#80)
    The following endpoints have been defined:
    
    * Recent videos (master feed).
      Location: /videos/feed
      Purpose: always syndicates the most recent 15 videos on this site.
    
    * Videos for a playlist
      Location: /playlist/:id/feed
      Purpose: always syndicates all the videos in a playlist.
    
    * Videos for a topic
      Location: /topics/:id/feed
      Purpose: always syndicates the most recent 15 videos part of a
      playlist in a particular topic on this site.
    
    Only the playlist feed syndicates the entire list of videos. Other feeds
    discard old items as new videos are published on the site.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,11 +1,21 @@
 class TopicsController < ApplicationController
-  
+
+  before_action :topic_set, only: [:show, :feed]
+
   def index
     @topics = Topic.all
   end
 
-  def show
-    @topic = Topic.friendly.find(params[:id])
+  def feed
+    @videos = Video.visible.joins(:playlist).includes(:playlist, playlist: [:topic]).where(playlists: { topic: @topic }).order(published_at: :desc).limit(15)
+    @updated_at = Video.order(updated_at: :desc).limit(1).pluck(:updated_at).first
+    render format: :xml, template: 'topics/feed.xml.erb', layout: false
   end
 
+  private
+
+  def topic_set
+    @topic = Topic.friendly.find(params[:id])
+    redirect_to topic_path(@topic) if params[:id] != @topic.slug
+  end
 end
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -17,4 +17,10 @@ class VideosController < ApplicationController
     authenticate_user! if @video.scheduled? && @video.private
     @in_playlist = @video.higher_items(5) + [@video] + @video.lower_items(5)
   end
+
+  def feed
+    @videos = Video.visible.includes(:playlist, playlist: [:topic]).order(published_at: :desc).limit(15)
+    @updated_at = Video.order(updated_at: :desc).limit(1).pluck(:updated_at).first
+    render format: :xml, template: 'videos/feed.xml.erb', layout: false
+  end
 end
