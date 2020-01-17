f8e2685bc9e081b2754b5376d1be4297e0368b7a
Dani Rodríguez <dani@danirod.es>
2020-01-17 06:47:59 +0100

    Correct Style/PercentLiteralDelimiters

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
 # Server to deploy to.
-server deploy_param(:server), user: deploy_param(:user), roles: %w{app db web}
+server deploy_param(:server), user: deploy_param(:user), roles: %w[app db web]
 
 # Deployment branch
 set :branch, deploy_param(:branch)
