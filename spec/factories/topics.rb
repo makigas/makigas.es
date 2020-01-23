e2a2d40d60ee4f80a027fe3e7f1eb11db35cae9c
Dani Rodríguez <dani@danirod.es>
2020-01-23 06:44:47 +0100

    Fix regressions while fixing mixins
    Related to 980a5f1e401705799f179ae2e5c9ee088e63e226

--- spec/factories/topics.rb
+++ spec/factories/topics.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :topic do
     title { 'Popular musics' }
     description { 'Popular musics of all kinds of styles and genres' }
-    thumbnail { fixture_file_upload(Rails.root.join('spec/fixtures/topic.png'), 'image/png') }
+    thumbnail { Rack::Test::UploadedFile.new('spec/fixtures/topic.png', 'image/png') }
     color { '#55ff55' }
   end
 end
