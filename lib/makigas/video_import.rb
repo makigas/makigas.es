50b92f6cea534d7ac3fc8c6fd848f08a68c94fb2
Dani Rodríguez <dani@danirod.es>
2020-03-17 19:52:59 +0100

    Address Layout/HashAlignment

--- lib/makigas/video_import.rb
+++ lib/makigas/video_import.rb
@@ -6,14 +6,14 @@ class Makigas::VideoImport
     @schema = YAML.load(File.read(schema))
     @schema.map do |playlist|
       pl = Playlist.create!(title: playlist["title"],
-        description: playlist["description"], slug: playlist["slug"],
-        youtube_id: playlist["youtube_id"])
+                            description: playlist["description"], slug: playlist["slug"],
+                            youtube_id: playlist["youtube_id"])
       playlist['videos'].each do |video|
         Video.create!(playlist: pl, title: video["title"],
-          description: video["description"], slug: video["slug"],
-          youtube_id: video["youtube_id"], duration: video["duration"],
-          position: video["position"],
-          created_at: Time.parse(video["created_at"]).utc)
+                      description: video["description"], slug: video["slug"],
+                      youtube_id: video["youtube_id"], duration: video["duration"],
+                      position: video["position"],
+                      created_at: Time.parse(video["created_at"]).utc)
       end
     end
   end
