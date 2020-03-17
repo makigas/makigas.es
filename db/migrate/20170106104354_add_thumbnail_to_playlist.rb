d029e7401cb1a424968faf31f73be8e33329e0a8
Dani Rodríguez <dani@danirod.es>
2020-03-17 20:39:15 +0100

    Layout/TrailingWhitespace

--- db/migrate/20170106104354_add_thumbnail_to_playlist.rb
+++ db/migrate/20170106104354_add_thumbnail_to_playlist.rb
@@ -2,7 +2,7 @@ class AddThumbnailToPlaylist < ActiveRecord::Migration[5.0]
   def up
     add_attachment :playlists, :thumbnail
   end
-  
+
   def down
     remove_attachment :playlists, :thumbnail
   end
--- db/migrate/20170106110213_add_thumbnail_to_topic.rb
+++ db/migrate/20170106110213_add_thumbnail_to_topic.rb
@@ -2,7 +2,7 @@ class AddThumbnailToTopic < ActiveRecord::Migration[5.0]
   def up
     add_attachment :topics, :thumbnail
   end
-  
+
   def down
     remove_attachment :topics, :thumbnail
   end
