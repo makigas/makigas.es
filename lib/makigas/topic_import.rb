b2d961217d26479d85c79ad433205e600acc351a
Dani Rodríguez <dani@danirod.es>
2020-03-22 18:59:20 +0100

    Address Security/YAMLLoad

--- lib/makigas/topic_import.rb
+++ lib/makigas/topic_import.rb
@@ -5,7 +5,7 @@ import 'yaml'
 module Makigas
   class TopicImport
     def initialize(schema)
-      @schema = YAML.load(File.read(schema))
+      @schema = YAML.safe_load(File.read(schema))
       @schema.each do |topic|
         Topic.create!(title: topic['title'], description: topic['description'], color: topic['color'])
       end
