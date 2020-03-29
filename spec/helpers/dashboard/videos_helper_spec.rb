d4dd1bdb5bf6a280f4bbbde243d73bfb56527a1f
Dani Rodríguez <dani@danirod.es>
2020-03-29 20:18:18 +0200

    spec/helpers/dashboard/videos_helper_spec.rb

--- spec/helpers/dashboard/videos_helper_spec.rb
+++ spec/helpers/dashboard/videos_helper_spec.rb
@@ -3,20 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe Dashboard::VideosHelper, type: :helper do
-  before { @video = FactoryBot.create(:video) }
+  let(:video) { FactoryBot.create(:video) }
 
   describe 'dashboard_video' do
-    it { expect(dashboard_video_path(@video)).to eq dashboard_playlist_video_path(@video, playlist_id: @video.playlist) }
-    it { expect(dashboard_video_url(@video)).to eq dashboard_playlist_video_url(@video, playlist_id: @video.playlist) }
+    it { expect(dashboard_video_path(video)).to eq dashboard_playlist_video_path(video, playlist_id: video.playlist) }
+    it { expect(dashboard_video_url(video)).to eq dashboard_playlist_video_url(video, playlist_id: video.playlist) }
   end
 
   describe 'move_dashboard_video' do
-    it { expect(move_dashboard_video_path(@video)).to eq move_dashboard_playlist_video_path(@video, playlist_id: @video.playlist) }
-    it { expect(move_dashboard_video_url(@video)).to eq move_dashboard_playlist_video_url(@video, playlist_id: @video.playlist) }
+    it { expect(move_dashboard_video_path(video)).to eq move_dashboard_playlist_video_path(video, playlist_id: video.playlist) }
+    it { expect(move_dashboard_video_url(video)).to eq move_dashboard_playlist_video_url(video, playlist_id: video.playlist) }
   end
 
   describe 'edit_dashboard_video' do
-    it { expect(edit_dashboard_video_path(@video)).to eq edit_dashboard_playlist_video_path(@video, playlist_id: @video.playlist) }
-    it { expect(edit_dashboard_video_url(@video)).to eq edit_dashboard_playlist_video_url(@video, playlist_id: @video.playlist) }
+    it { expect(edit_dashboard_video_path(video)).to eq edit_dashboard_playlist_video_path(video, playlist_id: video.playlist) }
+    it { expect(edit_dashboard_video_url(video)).to eq edit_dashboard_playlist_video_url(video, playlist_id: video.playlist) }
   end
 end
