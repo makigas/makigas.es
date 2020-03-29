414dff81a4e58dc7b40a18ca44df02f26ba62066
Dani Rodríguez <dani@danirod.es>
2020-03-29 19:46:41 +0200

    spec/features/topics/topic_page_spec.rb

--- spec/features/topics/topic_page_spec.rb
+++ spec/features/topics/topic_page_spec.rb
@@ -2,71 +2,67 @@
 
 require 'rails_helper'
 
-RSpec.feature 'Topic page', type: :feature do
-  before { @topic = FactoryBot.create(:topic) }
+RSpec.describe 'Topic page', type: :feature do
+  let(:topic) { FactoryBot.create(:topic) }
 
-  scenario 'shows information about a topic' do
-    visit topic_path(@topic)
-    expect(page).to have_text @topic.title
-    expect(page).to have_text @topic.description
-    expect(page).to have_css "img[src*='#{@topic.thumbnail.url(:small)}']"
+  it 'shows information about a topic' do
+    visit topic_path(topic)
+    expect(page).to have_text topic.title
+    expect(page).to have_text topic.description
+    expect(page).to have_css "img[src*='#{topic.thumbnail.url(:small)}']"
   end
 
-  scenario 'shows playlists in a topic' do
-    @playlist = FactoryBot.create(:playlist, topic: @topic)
+  it 'shows playlists in a topic' do
+    playlist = FactoryBot.create(:playlist, topic: topic)
 
-    visit topic_path(@topic)
+    visit topic_path(topic)
 
-    expect(page).to have_text @playlist.title
-    expect(page).to have_css "img[src*='#{@playlist.thumbnail.url(:default)}']"
-    expect(page).to have_css "a[href*='#{playlist_path(@playlist)}']"
+    expect(page).to have_text playlist.title
+    expect(page).to have_css "img[src*='#{playlist.thumbnail.url(:default)}']"
+    expect(page).to have_css "a[href*='#{playlist_path(playlist)}']"
   end
 
   context 'when the playlist is empty' do
-    before do
-      @playlist = FactoryBot.create(:playlist, topic: @topic)
-    end
+    let(:playlist) { FactoryBot.create(:playlist, videos: []) }
+    let(:topic) { FactoryBot.create(:topic, playlists: [playlist]) }
 
-    scenario 'shows the playlist length' do
-      visit topic_path(@topic)
+    it 'shows the playlist length' do
+      visit topic_path(topic)
       expect(page).to have_text '0 episodios'
     end
   end
 
   context 'when the playlist has a video' do
-    before do
-      @playlist = FactoryBot.create(:playlist, topic: @topic)
-      FactoryBot.create(:video, playlist: @playlist)
-    end
+    let(:video) { FactoryBot.create(:video) }
+    let(:playlist) { FactoryBot.create(:playlist, videos: [video]) }
+    let(:topic) { FactoryBot.create(:topic, playlists: [playlist]) }
 
     it 'shows the playlist length' do
-      visit topic_path(@topic)
+      visit topic_path(topic)
       expect(page).to have_text '1 episodio'
     end
   end
 
   context 'when the playlist has multiple videos' do
-    before do
-      @playlist = FactoryBot.create(:playlist, topic: @topic)
-      FactoryBot.create(:video, playlist: @playlist, youtube_id: '1234')
-      FactoryBot.create(:video, playlist: @playlist, youtube_id: '1235')
-    end
+    let(:first) { FactoryBot.create(:video, youtube_id: '1234') }
+    let(:second) { FactoryBot.create(:video, youtube_id: '1235') }
+    let(:playlist) { FactoryBot.create(:playlist, videos: [first, second]) }
+    let(:topic) { FactoryBot.create(:topic, playlists: [playlist]) }
 
-    scenario 'shows the playlist length' do
-      visit topic_path(@topic)
+    it 'shows the playlist length' do
+      visit topic_path(topic)
       expect(page).to have_text '2 episodios'
     end
   end
 
   context 'when the playlist has scheduled videos' do
-    before do
-      @playlist = FactoryBot.create(:playlist, topic: @topic)
-      FactoryBot.create(:video, playlist: @playlist, youtube_id: '1234')
-      FactoryBot.create(:video, playlist: @playlist, youtube_id: '1235', published_at: 2.days.from_now)
-    end
+    let(:first) { FactoryBot.create(:video, youtube_id: '1234') }
+    let(:second) { FactoryBot.create(:video, youtube_id: '1235', published_at: 2.days.from_now) }
+    let(:playlist) { FactoryBot.create(:playlist, videos: [first, second]) }
+    let(:topic) { FactoryBot.create(:topic, playlists: [playlist]) }
 
-    scenario 'scheduled videos are not counted' do
-      visit topic_path(@topic)
+    it 'scheduled videos are not counted' do
+      visit topic_path(topic)
       expect(page).to have_text '1 episodio'
     end
   end
