ffa6454e86019b7f5cc4ea4b6b2f4062a18f6812
Dani Rodríguez <dani@danirod.es>
2020-05-21 21:52:12 +0200

    Address issues

--- app/controllers/dashboard/links_controller.rb
+++ app/controllers/dashboard/links_controller.rb
@@ -1,55 +1,57 @@
 # frozen_string_literal: true
 
-class Dashboard::LinksController < Dashboard::DashboardController
-  before_action :video_set
+module Dashboard
+  class LinksController < Dashboard::DashboardController
+    before_action :video_set
 
-  def index
-    @links = @video.links.page(params[:page])
-  end
-
-  def new
-    @link = @video.links.build
-  end
-
-  def create
-    @link = @video.links.build(link_params)
-    if @link.save
-      redirect_to [:dashboard, @playlist, @video, :links], notice: t('.created')
-    else
-      render :new
+    def index
+      @links = @video.links.page(params[:page])
     end
-  end
 
-  def edit
-    @link = @video.links.find(params[:id])
-  end
-
-  def update
-    @link = @video.links.find(params[:id])
-    if @link.update(link_params)
-      redirect_to [:dashboard, @playlist, @video, :links], notice: t('.updated')
-    else
-      render :edit
+    def new
+      @link = @video.links.build
     end
-  end
 
-  def destroy
-    @link = @video.links.find(params[:id])
-    if @link.destroy
-      redirect_to [:dashboard, @playlist, @video, :links], notice: t('.destroyed')
-    else
-      redirect_to [:dashboard, @playlist, @video, :links], error: t('.error')
+    def create
+      @link = @video.links.build(link_params)
+      if @link.save
+        redirect_to [:dashboard, @playlist, @video, :links], notice: t('.created')
+      else
+        render :new
+      end
     end
-  end
 
-  private
+    def edit
+      @link = @video.links.find(params[:id])
+    end
 
-  def link_params
-    params.require(:link).permit(:name, :description, :url)
-  end
+    def update
+      @link = @video.links.find(params[:id])
+      if @link.update(link_params)
+        redirect_to [:dashboard, @playlist, @video, :links], notice: t('.updated')
+      else
+        render :edit
+      end
+    end
 
-  def video_set
-    @playlist = Playlist.friendly.find(params[:playlist_id])
-    @video = Video.friendly.find(params[:video_id])
+    def destroy
+      @link = @video.links.find(params[:id])
+      if @link.destroy
+        redirect_to [:dashboard, @playlist, @video, :links], notice: t('.destroyed')
+      else
+        redirect_to [:dashboard, @playlist, @video, :links], error: t('.error')
+      end
+    end
+
+    private
+
+    def link_params
+      params.require(:link).permit(:name, :description, :url)
+    end
+
+    def video_set
+      @playlist = Playlist.friendly.find(params[:playlist_id])
+      @video = Video.friendly.find(params[:video_id])
+    end
   end
 end
