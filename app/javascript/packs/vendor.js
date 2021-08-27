924652f5f1bc1d37d5b5cbf1fdc3dd9a3cb45bdf
Dani Rodríguez <dani@danirod.es>
2021-08-27 23:49:00 +0200

    fix glyphicons

--- app/javascript/packs/vendor.js
+++ app/javascript/packs/vendor.js
@@ -4,4 +4,5 @@ import 'cookieconsent';
 import 'rails-ujs';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'cookieconsent/build/cookieconsent.min.css';
