02f7a9c84395a9932128414f26d5335522ca8d79
Dani Rodríguez <dani@danirod.es>
2021-09-12 15:00:12 +0200

    rubo

--- app/models/transcription.rb
+++ app/models/transcription.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Transcription < Document
-  after_save :index_documentable
   after_destroy :index_documentable
+  after_save :index_documentable
 
   def index_documentable
     documentable.index! if documentable.respond_to?(:index!)
