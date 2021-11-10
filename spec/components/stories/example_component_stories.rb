9c75ae33fbd3bc27eddf1e6363b6bdc094527381
Dani Rodríguez <dani@danirod.es>
2021-11-10 22:14:53 +0100

    chore(six): delete unnecessary ExampleComponent

--- spec/components/stories/example_component_stories.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class ExampleComponentStories < ViewComponent::Storybook::Stories
-  layout 'storybook'
-
-  story :hello_world do
-    constructor(title: 'Hola buenos dias')
-  end
-end
