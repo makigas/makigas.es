7fdb716c490520a0464a99a89b311e552fc902a2
Dani Rodríguez <dani@danirod.es>
2021-11-10 23:24:43 +0100

    fix(six): fix removed component

--- app/javascript/packs/storybook.js
+++ app/javascript/packs/storybook.js
@@ -3,7 +3,6 @@ import "../six/layout/colors.scss";
 import "./storybook.scss";
 
 import "../six/components/discordcta";
-import "../six/components/example";
 import "../six/components/plhead";
 import "../six/components/plcard";
 import "../six/components/videocard";
