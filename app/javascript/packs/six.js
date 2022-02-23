6567882f885899a47c6de2f982fb2853d1bb28e2
Dani Rodríguez <dani@danirod.es>
2022-02-23 21:53:35 +0100

    components: expose Six Search Bar in Lookbook

---
+++ app/javascript/packs/six.js
@@ -0,0 +1,4 @@
+import '../six/layout/color.scss';
+import '../six/layout/responsive.scss';
+import '../six/layout/typography.scss';
+import '../six/components/search.scss';
---
+++ spec/components/previews/six/layout/search_component_preview.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Six
+  module Layout
+    # @label Media card
+    class SearchComponentPreview < ViewComponent::Preview
+      # Renders the search
+      #
+      # @param query text
+      # @param size select [normal, large]
+      # @display bgcolor '#222'
+      # @display padding 50px
+      def default(query: '', size: :normal)
+        render Six::Layout::SearchComponent.new(query: query.presence, size: size)
+      end
+    end
+  end
+end
