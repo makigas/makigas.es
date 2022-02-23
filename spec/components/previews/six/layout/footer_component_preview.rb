4cf8d6e0022a4d35aed2e4a80cdf46126c54800f
Dani Rodríguez <dani@danirod.es>
2022-02-23 23:01:17 +0100

    build: fix tests

--- spec/components/previews/six/layout/footer_component_preview.rb
+++ spec/components/previews/six/layout/footer_component_preview.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 module Six
-    module Layout
-      # @label Footer
-      class FooterComponentPreview < ViewComponent::Preview
-        # Renders a footer
-        def default
-          render Six::Layout::FooterComponent.new
-        end
+  module Layout
+    # @label Footer
+    class FooterComponentPreview < ViewComponent::Preview
+      # Renders a footer
+      def default
+        render Six::Layout::FooterComponent.new
       end
     end
   end
+end
