d943fa94026e917e4b160ca4b2f058ec65c37642
Dani Rodríguez <dani@danirod.es>
2022-03-07 19:12:27 +0100

    vendor: provide bootstrap via CDN

--- app/javascript/packs/vendor.js
+++ app/javascript/packs/vendor.js
@@ -1,18 +1,7 @@
-// TODO: Finish the migration to makigas v6 so that I can stop using jQuery!
-import jquery from 'jquery';
-window.$ = jquery;
-window.jQuery = jquery;
-
-// using require seems to be the only way to import bootstrap without having
-// jQuery fail. I need to migrate to makigas v6 to remove bootstrap 3 as soon
-// as possible!!!
-require('bootstrap/dist/js/bootstrap.min');
-
 import 'cookieconsent';
+import 'cookieconsent/build/cookieconsent.min.css';
 
 import Rails from '@rails/ujs';
 Rails.start()
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'cookieconsent/build/cookieconsent.min.css';
 
