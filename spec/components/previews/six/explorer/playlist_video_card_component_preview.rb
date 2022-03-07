4265f635021ce3a3b21b125a7a4296e6dea33163
Dani Rodríguez <dani@danirod.es>
2022-03-07 12:59:47 +0100

    six: refactor to avoid a rubocop error

--- spec/components/previews/six/explorer/playlist_video_card_component_preview.rb
+++ spec/components/previews/six/explorer/playlist_video_card_component_preview.rb
@@ -12,7 +12,9 @@ module Six
       def default
         component = Six::Explorer::PlaylistVideoCardComponent.new(video: Video.first)
         render ViewComponentContrib::WrapperComponent.new(component) do |wrapper|
-          "<div style='--playlist-lighten-30: #e1f5fe;'>#{wrapper.component}</div>".html_safe
+          wrapper.content_tag(:div, style: '--playlist-lighten-30: #e1f5fe;') do
+            wrapper.component
+          end
         end
       end
     end
