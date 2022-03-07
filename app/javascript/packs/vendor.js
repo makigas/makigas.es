9791d9049feabfa30ac5075e3b86eafc3b8d82bc
Dani Rodríguez <dani@danirod.es>
2022-03-07 19:18:41 +0100

    tooling: switch to esbuild (#667)

--- app/javascript/packs/vendor.js
+++ app/javascript/packs/vendor.js
@@ -1,9 +1,7 @@
-import 'jquery/src/jquery';
-import 'bootstrap';
 import 'cookieconsent';
-import Rails from '@rails/ujs';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'cookieconsent/build/cookieconsent.min.css';
 
+import Rails from '@rails/ujs';
 Rails.start()
+
+
--- config/webpack/app-config.js
+++
@@ -1,8 +0,0 @@
-const webpack = require('webpack');
-
-module.exports = [
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery'
-  })
-];
