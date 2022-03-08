ed8958c79fead6558e4e714f6da201ad3afb6664
Dani Rodríguez <dani@danirod.es>
2022-03-08 18:11:37 +0100

    six: design the about pages
    To view the design, it will be necessary to use the USE_SIX environment
    variable with a value set (for instance, to 1).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  layout('six/six') if ENV['USE_SIX'].present?
 end
--- app/javascript/packs/six.js
+++ app/javascript/packs/six.js
@@ -1,8 +1,11 @@
+import "normalize.css";
+
 import "../six/components/navbar";
 
 import "../six/layout/colors.scss";
 import "../six/layout/responsive.scss";
 import "../six/layout/typography.scss";
+import "../six/layout/base.scss";
 import "../six/components/button.scss";
 import "../six/components/discordcta.scss";
 import "../six/components/explorer.scss";
