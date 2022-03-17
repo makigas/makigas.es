3c35898c8cb65ea6cfc4180249df97d448f01db0
Dani Rodríguez <dani@danirod.es>
2022-03-17 22:32:57 +0100

    config: use AUTHORIZED_HOSTS for additional dev hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,5 +75,7 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.hosts << ENV["AUTHORIZED_HOST"] if ENV["AUTHORIZED_HOST"].present?
+  if ENV["AUTHORIZED_HOSTS"].present?
+    ENV["AUTHORIZED_HOSTS"].split(",").each { |h| config.hosts << h }
+  end
 end
