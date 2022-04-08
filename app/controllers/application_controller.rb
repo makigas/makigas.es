efaa935e8b70f74f9d432036eb330845a24c070e
Dani Rodríguez <dani@danirod.es>
2022-04-08 23:48:01 +0200

    six: remove references to old layouts in pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  layout('six/six') if ENV['USE_SIX'].present?
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,4 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-  layout('six/pages') if ENV['USE_SIX'].present?
-
-  def discord
-    render(layout: 'six/six') if ENV['USE_SIX'].present?
-  end
 end
