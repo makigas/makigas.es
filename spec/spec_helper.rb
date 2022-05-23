433e73c93031e7a122df0f6caa821ce5f2a15fcc
Dani Rodríguez <dani@danirod.es>
2022-05-23 13:41:39 +0200

    chore(deps): update to Ruby 3.1
    This commit updates the framework to Ruby 3.1 and I hope I haven't
    forgot anything. It also bumps the Docker images, which have been
    tested, and includes all the CI pipelines that expect the Ruby version
    to stay the same.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,10 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  # Smooth like butter
+  config.add_formatter 'Fuubar'
+  config.fuubar_output_pending_results = false
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
