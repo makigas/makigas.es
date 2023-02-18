da337f590db537d68a1abf8ef644b66cb7b4bca4
Dani Rodríguez <dani@danirod.es>
2023-02-18 17:28:36 +0100

    feat: add rel-next and rel-prev to the video page buttons

--- app/components/six/base/button_component.rb
+++ app/components/six/base/button_component.rb
@@ -3,17 +3,18 @@
 module Six
   module Base
     class ButtonComponent < ViewComponent::Base
-      def initialize(href: nil, classes: nil, id: nil, text: nil)
+      def initialize(href: nil, classes: nil, id: nil, text: nil, rel: nil)
         super
         @href = href
         @classes = classes
         @id = id
         @text = text
+        @rel = rel
       end
 
       private
 
-      attr_reader :href, :classes, :id, :text
+      attr_reader :href, :classes, :id, :text, :rel
 
       def class_list
         base_classes = ['btn']
