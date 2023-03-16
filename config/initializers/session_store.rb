2b7a5508861be64b38ae01a10493bc62b8a52693
Dani Rodríguez <dani@danirod.es>
2023-03-16 21:59:23 +0100

    feat: detect dashboard user when logged in
    So that I can disable Plausible for my own views.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,5 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_makigas_session', secure: Rails.env.production?
+Rails.application.config.session_store :cookie_store, key: '_makigas_session', domain: :all,
+                                                      secure: Rails.env.production?
