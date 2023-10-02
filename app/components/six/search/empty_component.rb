91a0fb1963f5ff0047f25503abad24feb31077ab
Dani Rodríguez <dani@danirod.es>
2023-10-02 14:02:38 +0200

    feat: include link to YouTube search when no results
    In case an user search for something and gets 0 results, include a link
    to YouTube where the search can be made to look for content that may
    have been uploaded but not added to the site, or to use their search
    engine to look for something that I cannot index yet.

--- app/components/six/search/empty_component.rb
+++ app/components/six/search/empty_component.rb
@@ -3,6 +3,26 @@
 module Six
   module Search
     class EmptyComponent < ViewComponent::Base
+      def initialize(filters: {})
+        super
+        @filters = filters
+      end
+
+      private
+
+      attr_reader :filters
+
+      def emojis
+        %w[😔 😖 😣 ☹️ 😢 😓]
+      end
+
+      def search_url
+        "https://www.youtube.com/@makigas/search?query=#{search_query}"
+      end
+
+      def search_query
+        [filters[:q], filters[:tag]].flatten.compact.join(' ')
+      end
     end
   end
 end
