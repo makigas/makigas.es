041880317a68d245428ef05312f0a5dc56196642
Dani Rodríguez <dani@danirod.es>
2023-10-17 19:29:38 +0200

    feat: the Discord CTA button should jump into the server

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
+  def discord
+    return unless params.keys.include?('go')
+
+    discord_url = "https://discord.gg/#{Rails.application.secrets.discord_server_invite}"
+    redirect_to discord_url, status: :found, allow_other_host: true
+  end
 end
