bb2a7e56b5fd55cb14c8463733bc8e8b53171eca
Dani Rodríguez <dani@danirod.es>
2024-08-15 19:37:03 +0200

    Show information about a tag in the search page

---
+++ app/components/six/search/tag_information_component.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Six
+  module Search
+    class TagInformationComponent < ViewComponent::Base
+      def initialize(tag:)
+        super
+        @tag = tag
+      end
+    end
+  end
+end
