aa616c1ffa5568b9b3cedebbfe2d95285c0d732b
Dani Rodríguez <dani@danirod.es>
2024-08-16 12:06:52 +0200

    Fix regression after removing topics from navbar
    It seems that this test was catching the topic from the navigation bar.
    Once the navigation bar does not present topics anymore, now this test
    fails because the only other match happens in the main area.

--- spec/features/front/front_page_spec.rb
+++ spec/features/front/front_page_spec.rb
@@ -14,7 +14,7 @@ RSpec.feature 'Front page' do
   end
 
   scenario 'allows the user to navigate for topics' do
-    topic = create(:topic, slug: 'mejora-tus-skills')
+    topic = create(:topic, title: 'Skills nuevas', slug: 'mejora-tus-skills')
     visit root_path
     expect(page).to have_text topic.title
   end
