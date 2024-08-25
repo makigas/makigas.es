520bc331d322cc748f2c09f2ab7f386dd2938907
Dani Rodríguez <dani@danirod.es>
2024-08-25 14:26:50 +0200

    Enhance dashboard cookies in development mode
    This commit allows developer mode to set cookies without a subdomain
    using lvh.me as the hostname. This allows to detect if an editor has
    logged in from the main application when running in development mode as
    well.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,5 +2,9 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_makigas_session', domain: :all,
-                                                      secure: Rails.env.production?
+DOMAINS = {
+  'production' => '.makigas.es',
+  'development' => '.lvh.me'
+}.freeze
+
+Rails.application.config.session_store :cookie_store, key: '_makigas_session', domain: DOMAINS[Rails.env] || :all
