d2d71fa17b883ea7108bf82bf4ce00476c316e62
Dani Rodríguez <dani@danirod.es>
2024-09-07 14:03:54 +0200

    Show popular playlists in the site footer
    I'll check in the following days to see if having this query on every
    page load causes issues or if it is safe to keep. Telemetry is ready to
    check.

--- app/components/six/layout/footer_component.rb
+++ app/components/six/layout/footer_component.rb
@@ -3,6 +3,23 @@
 module Six
   module Layout
     class FooterComponent < ViewComponent::Base
+      def popular_playlists
+        popular_playlists_by_index
+      end
+
+      # The playlists whose playlist page has the more views.
+      def popular_playlists_by_index
+        Playlist.where.not(views_recent: nil).where(exclude_from_search: false,
+                                                    deprecated: false).order(views_recent: :desc).take(5)
+      end
+
+      # The playlists whose content is the most popular.
+      def popular_playlists_by_video
+        Video
+          .group('playlist_id')
+          .select('sum(views_recent) as total, playlist_id')
+          .order(total: :desc)
+      end
     end
   end
 end
