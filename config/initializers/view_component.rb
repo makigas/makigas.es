cd01b36832c2cedc0bcfffe4532eb53862c2503c
Dani Rodríguez <dani@danirod.es>
2025-05-11 19:13:41 +0200

    Skip configuring ViewComponent in production mode

--- config/initializers/view_component.rb
+++ config/initializers/view_component.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 # Configure ViewComponent preview paths.
-Rails.application.config.view_component.default_preview_layout = 'component_preview'
-Rails.application.config.view_component.preview_paths = [Rails.root.join('spec/components/previews')]
+unless Rails.env.production?
+  Rails.application.config.view_component.default_preview_layout = 'component_preview'
+  Rails.application.config.view_component.preview_paths = [Rails.root.join('spec/components/previews')]
+end
